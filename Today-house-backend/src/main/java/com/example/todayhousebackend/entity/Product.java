cb6c506b756fdaafeb10ad2cc6ea33c5b5bcc65c
Yonghyun Lee <dldydgus95426@gmail.com>
2023-03-25 02:59:13 +0900

    no message

--- Today-house-backend/src/main/java/com/example/todayhousebackend/entity/Product.java
+++ Today-house-backend/src/main/java/com/example/todayhousebackend/entity/Product.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
 
     @Column(updatable = false)
