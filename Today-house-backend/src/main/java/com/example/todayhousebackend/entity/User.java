5bc84832b95f81d5b39502ea28d69d5d158b22f4
yujin-17 <lyjyellow@gmail.com>
2023-03-24 19:29:28 +0900

    entity modified

---
+++ Today-house-backend/src/main/java/com/example/todayhousebackend/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.todayhousebackend.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "USERS")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long Id;
+
+  @Column(nullable = false)
+  private String loginId;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String email;
+
+
+}
