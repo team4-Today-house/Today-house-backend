284af9c74d5e1e0ca83b1a0bcd6e1cdfee4e4980
yujin-17 <lyjyellow@gmail.com>
2023-03-25 03:48:54 +0900

    로그인 회원가입 response 수정

--- Today-house-backend/src/main/java/com/example/todayhousebackend/controller/UserController.java
+++ Today-house-backend/src/main/java/com/example/todayhousebackend/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.todayhousebackend.dto.SignupRequestDto;
 import com.example.todayhousebackend.exception.ExceptionEnum;
 import com.example.todayhousebackend.jwt.JwtUtil;
 import com.example.todayhousebackend.service.UserService;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,19 +25,22 @@ public class UserController {
   private final UserService userService;
 
   @PostMapping("/api/user/signup")
-  public ResponseEntity<ExceptionEnum> signup(@RequestBody @Valid SignupRequestDto dto){
+  public ResponseEntity<Map<String, String>> signup(@RequestBody @Valid SignupRequestDto dto){
 
     userService.signup(dto);
+    Map<String, String> response = new HashMap<>();
+    response.put("회원가입 성공", String.valueOf(HttpStatus.OK));
 
-    return new ResponseEntity("회원가입 성공", HttpStatus.OK);
+    return ResponseEntity.ok(response);
   }
 
   @PostMapping("/api/user/login")
-  public ResponseEntity<Object> login(@RequestBody SignupRequestDto dto, HttpServletResponse response){
+  public ResponseEntity<Map<String, String>> login(@RequestBody SignupRequestDto dto, HttpServletResponse response){
     String token = userService.login(dto);
-
     response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
-    return new ResponseEntity("로그인 성공", HttpStatus.OK);
+    Map<String, String> res = new HashMap<>();
+    res.put("회원가입 성공", String.valueOf(HttpStatus.OK));
+    return ResponseEntity.ok(res);
   }
 
 
