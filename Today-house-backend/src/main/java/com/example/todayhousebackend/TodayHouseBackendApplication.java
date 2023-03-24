02d3d65f67ec834eabe4bf6b30368654573a4b54
yujin-17 <lyjyellow@gmail.com>
2023-03-24 18:33:08 +0900

    Initial Environment Setting

---
+++ Today-house-backend/src/main/java/com/example/todayhousebackend/TodayHouseBackendApplication.java
@@ -0,0 +1,17 @@
+package com.example.todayhousebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJpaAuditing
+@EnableScheduling
+@SpringBootApplication
+public class TodayHouseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayHouseBackendApplication.class, args);
+	}
+
+}
---
+++ Today-house-backend/src/test/java/com/example/todayhousebackend/TodayHouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todayhousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayHouseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
