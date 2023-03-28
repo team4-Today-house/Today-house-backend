a3f32aff4aeca88c6e8eb253fdcd38c748ec1f48
yujin-17 <lyjyellow@gmail.com>
2023-03-29 02:44:23 +0900

    fix: kakaoLogin

--- Today-house-backend/src/main/java/com/example/todayhousebackend/controller/UserController.java
+++ Today-house-backend/src/main/java/com/example/todayhousebackend/controller/UserController.java
@@ -52,11 +52,12 @@ public class UserController {
   public ResponseEntity kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
     // code: 카카오 서버로부터 받은 인가 코드
     String createToken = kakaoService.kakaoLogin(code, response);
+    response.addHeader(JwtUtil.AUTHORIZATION_HEADER, createToken);
 
     // Cookie 생성 및 직접 브라우저에 Set
-    Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
-    cookie.setPath("/");
-    response.addCookie(cookie); // addCookie 를 사용해 이 안에 cookie를 서버에서 넣어줄 수 있다.
+//    Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
+//    cookie.setPath("/");
+//    response.addCookie(cookie); // addCookie 를 사용해 이 안에 cookie를 서버에서 넣어줄 수 있다.
     // 클라이언트에서 직접 쿠키를 받아서 쿠키 저장소에 저장하지 않아도 자동으로 쿠키 저장소에 저장.
 
     return ResponseEntity.ok().body("카카오로그인");
