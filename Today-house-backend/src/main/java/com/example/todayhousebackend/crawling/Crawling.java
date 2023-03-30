d1e6da331a6d2526e0b879cb6b70e4b412997d8e
yujin-17 <lyjyellow@gmail.com>
2023-03-30 12:36:02 +0900

    merge 수정

--- Today-house-backend/src/main/java/com/example/todayhousebackend/crawling/Crawling.java
+++ Today-house-backend/src/main/java/com/example/todayhousebackend/crawling/Crawling.java
@@ -27,7 +27,6 @@ public class Crawling {
     Elements contentsPrice = doc.getElementsByAttributeValue("class","production-item-price__price");
     // FIXME : Check
     Elements img = doc.getElementsByAttributeValue("class" , "image");
-//    Elements img = doc.select("#store-index > section.container.store-index-section.store-index-today-deal-list > div > div.col-12  > div.today-deal-item--wrapper > article.today-deal-item > div.today-deal-item__image > div.today-deal-item__image__item > div.production-item-image > img");
 
     List<Map<String, String>> result = new ArrayList<>();
     for (int i = 0; i < title.size(); i++) {
